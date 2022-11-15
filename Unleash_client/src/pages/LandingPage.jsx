694543d1326e37aaaaff89f7255455b5b2e604d6
EthanKIM <khg023246@gmail.com>
2022-11-15 20:41:29 +0900

    feat: basic landingpage

--- Unleash_client/src/pages/LandingPage.jsx
+++ Unleash_client/src/pages/LandingPage.jsx
@@ -1,9 +1,17 @@
-function LandingPage() {
-    return (
-      <div>
+import React from "react";
+import '../resources/css/App.css'
+import image from '../resources/image/4.jpg'
+
+const LandingPage = () => {
+  return (
+    <>
+      <div className="landing">
+        <h1 className="landing_logo">𝙐𝙣𝙡𝙚𝙖𝙨𝙝</h1>
+        <img className="landing_bgImg" src={image} alt=""/>
+        <button className="landing_button">Unleash your travel</button>
       </div>
-    );
-  }
-  
-  export default LandingPage;
-  
+    </>
+  );
+};
+
+export default LandingPage;
