3252d8b61840ff49ff0baa288a5de485b2c6b4ac
EthanKIM <khg023246@gmail.com>
2022-11-16 12:20:30 +0900

    feat: landingpage gradiant

--- Unleash_client/src/pages/LandingPage.jsx
+++ Unleash_client/src/pages/LandingPage.jsx
@@ -3,14 +3,23 @@ import '../resources/css/App.css'
 
 const LandingPage = () => {
   const [bgImg, setBgImg] = useState('landing_bgImg');
+
   const handleLogoChange = () => {
     setBgImg('landing_bgImg1')
+  }
+  const handleMouseOver = (e) => {
+    let rect = e.target.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    e.target.style.setProperty('--x', x + 'px');
+    e.target.style.setProperty('--y', y + 'px');
   }
   return (
     <>
       <div className="landing">
         <h1 className="landing_logo">𝙐𝙣𝙡𝙚𝙖𝙨𝙝</h1>
         <div className={bgImg} />
+        <div className="landing_gradiant_h"onMouseMove={handleMouseOver} />
         <button className="landing_button" onClick={handleLogoChange}>Unleash your travel</button> {/*버튼을 누르면 bgImg classanme이 bgImg1로 바뀜. */}
       </div>
     </>
