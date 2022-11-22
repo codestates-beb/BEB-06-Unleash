dd0700b807009f939dbb45ca0001e8c371ab0784
sinsu <sinsu19012@gmail.com>
2022-11-22 16:34:05 +0900

    style: airbnb 코드 컨벤션 적용

--- Unleash_server/sequelize/dbload.js
+++ Unleash_server/sequelize/dbload.js
@@ -1,5 +1,7 @@
-require("dotenv").config();
-const SequelizeAuto = require("sequelize-auto");
+require('dotenv').config();
+
+const SequelizeAuto = require('sequelize-auto');
+
 const auto = new SequelizeAuto(
   process.env.DB_NAME,
   process.env.USER_NAME,
@@ -7,7 +9,7 @@ const auto = new SequelizeAuto(
   {
     host: process.env.DB_HOST, // DB Host 주소
     port: process.env.DB_PORT, // 포트 번호
-    dialect: "mysql", // 사용하는 DBMS 종류
+    dialect: 'mysql', // 사용하는 DBMS 종류
   }
 );
 
