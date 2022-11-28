83f5ba17d82d1f0c3c1104bdf4d033459fef96bb
EthanKIM <khg023246@gmail.com>
2022-11-28 14:05:06 +0900

    feat: marketplace 폼 쿼리 구상중

--- Unleash_client/src/components/MarketPlace_components/MarketPlaceAside.jsx
+++ Unleash_client/src/components/MarketPlace_components/MarketPlaceAside.jsx
@@ -2,9 +2,11 @@ import React, {useState, useContext} from "react";
 import MarketPlaceCalander from "./MarketPlaceCalander";
 import { TestContext } from "../../resources/context_store/Test";
 import axios from "axios"
+import { ListContext } from "../../resources/context_store/ListContext";
 
 const MarketPlaceAside = () => {
   const context = useContext(TestContext);
+  const conetext2 = useContext(ListContext);
   // 해야할거, sidebar glow effect, postion 지정,
   // 여기서 선택하는거 state 로 올리기. form태그.
   // 날짜?
@@ -14,13 +16,17 @@ const MarketPlaceAside = () => {
   const [city, setCity] = useState('');
   const [seatClass, setSeatClass] = useState('');
   const [departDate, setDepartDate] = useState('');
-  const [returnDate, setReturnDate] = useState('');
+  const cities = ["Paris", "NewYork", "Osaka", "Sydney", "Roma"]
+  const [bg, setBg] = useState(false);
 
   const handleDButtonClick = (e) => {
     setIsOpen(() => !isOpen);
   }
   const handleSubmit = () => {
-    const filter = {city: city, seatClass: seatClass, departDate: departDate, returnDate: returnDate}
+    const filter = {city: city, seatClass: seatClass, departDate: departDate,}
+  }
+  const handleClick = (e) => {
+    setBg(() => !bg);
   }
   
   // 전체 도시 리스트 완성되면 , 배열 만들어서 46번째 li부분 map처리.
@@ -31,12 +37,8 @@ const MarketPlaceAside = () => {
           <form className="marcketplace_contents_sidebar_container" onSubmit={handleSubmit}>
               <button type="button" className="marketplace_contents_collapse" onClick={handleDButtonClick}>Set Destination</button>
               {isOpen &&
-              <ul className="marketplace_contents_destination">
-                <li><p>city</p></li>
-                <li><p>NewYork</p></li>
-                <li><p>Osaka</p></li>
-                <li><p>Australia</p> </li>
-                <li><p>Roma</p></li>
+              <ul className="marketplace_contents_destination" onClick={handleClick}>
+                {cities.map((item, idx) => <li key={idx} style={bg? {backgroundColor: "blue"} : {}}><p>{item}</p></li>)}
               </ul>}
             <MarketPlaceCalander />
             <button className="marketplace_search" type="submit">search</button>
