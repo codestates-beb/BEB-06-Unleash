781ce03b6c18fb97be7d1ff4d9af33c79bceadfa
sinsu <sinsu19012@gmail.com>
2022-12-01 00:12:44 +0900

    feat: 저장

--- Unleash_server/controller/dbload.controller.js
+++ Unleash_server/controller/dbload.controller.js
@@ -7,7 +7,23 @@ const marketAbi = require("../initialSignSet/contract/MarketAbi.json");
 
 const test = async () => {
   const unleashAddress = "0xB7c26E7F3d7AE71cE62A97Edc59Fe4F4d94AAA3D";
-  const umarketAddress = "0xB7c26E7F3d7AE71cE62A97Edc59Fe4F4d94AAA3D";
+  const marketAddress = "0xB7c26E7F3d7AE71cE62A97Edc59Fe4F4d94AAA3D";
+  const privateKey = process.env.PRIVATE_KEY;
+  const provider = ethers.providers.getDefaultProvider({
+    name: "goerli",
+    chainId: 5,
+  });
+  const walletWithProvider = new ethers.Wallet(privateKey, provider);
+  const unleashContract = new ethers.Contract(
+    unleashAddress,
+    unleashAbi,
+    walletWithProvider
+  );
+  const marketContract = new ethers.Contract(
+    marketAddress,
+    marketAbi,
+    walletWithProvider
+  );
 };
 
 module.exports = {
