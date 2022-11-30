deeb7aa50af5711850b774584e037fbb547aafdd
EthanKIM <khg023246@gmail.com>
2022-11-30 13:57:14 +0900

    feat: marketplace filter sidebar

--- Unleash_client/src/components/MarketPlace_components/MarketPlaceAside.jsx
+++ Unleash_client/src/components/MarketPlace_components/MarketPlaceAside.jsx
@@ -2,20 +2,20 @@ import React, {useState, useContext} from "react";
 import MarketPlaceCalander from "./MarketPlaceCalander";
 import { TestContext } from "../../resources/context_store/Test";
 import axios from "axios"
+import { useNavigate } from "react-router-dom";
 import { ListContext } from "../../resources/context_store/ListContext";
 
 const MarketPlaceAside = () => {
   const context = useContext(TestContext);
   const conetext2 = useContext(ListContext);
-  // 해야할거, sidebar glow effect, postion 지정,
-  // 여기서 선택하는거 state 로 올리기. form태그.
-  // 날짜?
+  const navigate = useNavigate();
   const {isOpen, setIsOpen, isOpen2, setIsOpen2} = context;
+  const {setList} = conetext2;
+  const ToBox = { Roma : "FCO" , Osaka : "ITM" , Sydney : "SYD" , NewYork : "JFK" , Paris: "CDG"}
 
   // 아래의 내용은 context 에서 전역으로 설정되서 온 것을 바꿔주면됨.
   const [city, setCity] = useState('');
   const [departDate, setDepartDate] = useState(new Date('12/01/2022'));
-  
 
   const cities = ["Paris", "NewYork", "Osaka", "Sydney", "Roma"]
   const [bg, setBg] = useState(false);
@@ -25,14 +25,33 @@ const MarketPlaceAside = () => {
   }
   const handleSubmit = (e) => {
     e.preventDefault();
-    const filter = {city: city, departDate: departDate}
-    console.log(filter)
+
+    if (!city || !departDate) return alert("도시와 출발일자를 선택해주세요.")
+    let filter = {city: city, departDate: departDate}
+    let To = ToBox[filter.city];
+    let params = {  "from" : "ICN" };
+    params["to"] = To;
+    params["departuretime"] = new Date(departDate.getTime() - (departDate.getTimezoneOffset() * 60000)).toISOString().substr(0, 11);
+    axios.get('http://localhost:5001/marketplace/ticket', {params} )
+    .then(res => {
+      const data = res.data;
+      const list = JSON.stringify([...data]);
+      console.log(list)
+      localStorage.setItem("marketList", list);
+      setList(() => res.data);
+      navigate("/marketplace")
+      window.location.reload();
+    })
+    .catch(e => {
+      console.log(e);
+      return e;
+    })
   }
   const handleClick = (e) => {
+    console.log(bg)
     setBg(() => !bg);
     setCity(e.target.textContent)
   }
-  
   // 전체 도시 리스트 완성되면 , 배열 만들어서 46번째 li부분 map처리.
   return (
     
@@ -41,8 +60,8 @@ const MarketPlaceAside = () => {
           <form className="marcketplace_contents_sidebar_container" onSubmit={handleSubmit}>
               <button type="button" className="marketplace_contents_collapse" onClick={handleDButtonClick}>Set Destination</button>
               {isOpen &&
-              <ul className="marketplace_contents_destination" onClick={handleClick}>
-                {cities.map((item, idx) => <li key={idx} style={bg? {backgroundColor: "blue"} : {}}><p>{item}</p></li>)}
+              <ul className="marketplace_contents_destination" >
+                {cities.map((item, idx) => <li onClick={handleClick} className={bg ? "active_destination_list" : ""} key={idx}><p>{item}</p></li>)}
               </ul>}
             <MarketPlaceCalander departDate={departDate} setDepartDate={setDepartDate}/>
             <button className="marketplace_search" type="submit">search</button>
